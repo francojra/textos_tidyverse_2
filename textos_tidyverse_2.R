6353d46c8d3e677c4d468ab911bc589395f5a4c7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-01 23:33:53 -0300

    Add files via upload

--- textos_tidyverse_2.R
+++ textos_tidyverse_2.R
@@ -7,5 +7,25 @@
 # Pacotes necessário ------------------------------------------------------------------------------------------------------------------------
 
 library(tidyverse)
-library(tidytext)
-library(tm)
+library(tidytext) # Pacote para análise de textos
+library(tm) # Pacote para text mining
+
+# Carregando texto -------------------------------------------------------------------------------------------------------------------------
+
+casmurro <- scan("casmurro.txt", what = "character", sep = "\n", encoding = "UTF-8")
+
+# Manipulando o texto ----------------------------------------------------------------------------------------------------------------------
+
+casmurro[1:30] # Início do texto, até as 30 primeiras linhas.
+grep("FIM", casmurro) # Procura a palavra "fim" no texto
+
+casmurro[27:6393] # Linha onde começa o capítulo até o fim do texto
+
+stopwords("pt") # Indica algumas palavras em português ("pt") que não tem muita utilidade 
+## para as análises
+
+casmurro <- removeWords(casmurro, stopwords("pt")) # Remove palavras desnecessárias
+
+# Análise do texto -------------------------------------------------------------------------------------------------------------------------
+
+grep("Bentinho", casmurro)
